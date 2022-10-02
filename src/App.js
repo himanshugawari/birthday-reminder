d4fc90e6a9cf9620a1b06d64c96ab42948dcac9c
himanshugawari <himanshu25031991@gmail.com>
2022-10-02 13:07:15 +0530

    2 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import List from './List';
+import data from './data';
 
 export default function App() {
+  const [people, setPeople] = React.useState(data);
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <main>
+      <section className="container">
+        <h3>{people.length} Birthday's Today</h3>
+        <List people={people} setPeople={setPeople} />
+        <button onClick={() => setPeople([])}>Clear All</button>
+      </section>
+    </main>
   );
 }
